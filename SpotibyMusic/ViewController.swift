35832eeafd84eba984a3a3063d990034dd5354ab
juAlberti <julia.maia@acad.pucrs.br>
2021-06-21 14:43:43 -0300

    first commit

---
+++ SpotibyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotibyMusic
+//
+//  Created by Julia Alberti Maia on 21/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
