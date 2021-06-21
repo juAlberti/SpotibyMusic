4e63f1dec8736cec9489d4ecb20661c2dc1be39f
juAlberti <julia.maia@acad.pucrs.br>
2021-06-21 15:26:45 -0300

    Adding files

---
+++ SpotibyMusic/AlbumInfo.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumInfo.swift
+//  SpotibyMusic
+//
+//  Created by Julia Alberti Maia on 21/06/21.
+//
+
+import UIKit
+
+class AlbumInfo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
