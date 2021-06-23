fc09af304baa361908cd0f80772209f9b6895658
Luiz Eduardo <luizdudumr@gmail.com>
2021-06-23 15:56:42 -0300

    branch added

--- SpotibyMusic/AlbumPlaylistDetails.swift
+++ SpotibyMusic/AlbumPlaylistDetails.swift
@@ -7,23 +7,78 @@
 
 import UIKit
 
-class AlbumPlaylistDetails: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AlbumPlaylistDetails: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albumDetails?.musics.count ?? 1
+        
     }
     
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let album = albumDetails?.musics[indexPath.row]
+        
+        
+        if (indexPath.row == 0){
+            let firstCell = tableView.dequeueReusableCell(withIdentifier: "first-cell", for: indexPath) as! AlbumDetailsCell
+            
+            if let ismusic = album, let albumdetails = albumDetails {
+                // configurar primeira celular
+                firstCell.coverImage.image = UIImage(named: ismusic.id)
+                firstCell.title.text = ismusic.title
+                firstCell.subtitle.text = "\(albumdetails.type) by \(ismusic.artist)"
+                firstCell.numberOfSongs.text = "\(albumdetails.musics.count) songs"
+                
+                
+                //date
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "MMM d, yyyy"
+                
+                var dateFromStr = dateFormatter.string(from: albumdetails.referenceDate)
+                firstCell.releasedAt.text = "Released \(dateFromStr)"
+            }
+            
+            
+            
+            
+            return firstCell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "others-cell2", for: indexPath) as! AlbumDetailsOthersCell
+        
+        if let ismusic = album, let albumdetails = albumDetails {
+            // configurar a celulas gerais
+            cell.coverImage.image = UIImage(named: ismusic.id)
+            
+            if (albumdetails.type == .album){
+                cell.title.text = ismusic.title
+                cell.subtitle.text = ismusic.artist
+                
+            }
+            
+            else if (albumdetails.type == .playlist){
+                cell.title.text = ismusic.title
+                cell.subtitle.text = ismusic.artist
+            }
+        }
+        return cell
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //private var musicService: MusicService? = try? MusicService()
+    var albumDetails: MusicCollection?
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //
+    }
 }
--- SpotibyMusic/Library.swift
+++ SpotibyMusic/Library.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
-
-class Library: UIViewController {
-
+//card-detail
+class Library: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var musicService: MusicService? = try? MusicService()
+    private var albumPlaylistCollection: [MusicCollection] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        albumPlaylistCollection = musicService?.loadLibrary() ?? []
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albumPlaylistCollection.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "others-cell", for: indexPath) as! LibraryCellsTableViewCell
+        
+        // configurar a celula
+        let album = albumPlaylistCollection[indexPath.row]
+        
+        cell.coverImage.image = UIImage(named: album.id)
+        cell.title.text = album.title
+        cell.subtitle.text = "\(album.type) · \(album.mainPerson)"
+        
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "albumDetails", sender: indexPath)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "albumDetails", let indexPath = sender as? IndexPath {
+            let destination = segue.destination as? AlbumPlaylistDetails
+            destination?.albumDetails = albumPlaylistCollection[indexPath.row]
+        }
+    }
 }
